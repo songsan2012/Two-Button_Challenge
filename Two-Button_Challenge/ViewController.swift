3685316491095c3ce2210f66bb32dd3e1f0278e2
Song San <songsan2012@gmail.com>
2021-12-23 21:17:10 -0800

    First Commit to set up Program

--- Two-Button_Challenge/ViewController.swift
+++ Two-Button_Challenge/ViewController.swift
@@ -9,12 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    
+        print("Form loaded")
+        messageLabel.text = ""
+    
     }
 
-
+    // -- To Show a Message
+    @IBAction func showMessagePressed(_ sender: Any) {
+        
+        print("😎 Show Message Pressed.")
+    }
+    
+    // -- To Show Another Message
+    @IBAction func showAnotherMessagePressed(_ sender: Any) {
+        
+        print("☺️ Show Another Message Pressed.")
+    }
+    
+    
 }
 
